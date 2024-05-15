0c2040b6c895ab1af94a4926b31eca13029f2eaa
Fernando Xavier da Silva <fernando.xavier@cnj.jus.br>
2024-05-15 10:10:38 -0300

    atualização curso básico

--- cypress/e2e/CAC.TAT.spec.cy.js
+++ cypress/e2e/CAC.TAT.spec.cy.js
@@ -84,8 +84,12 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.get('.error').should('be.visible')
   })
   it('envia o formuário com sucesso usando um comando customizado', function () {
+    cy.clock()
     cy.fillMandatoryFieldsAndSubmit()
     cy.get('.success').should('be.visible')
+    cy.tick(3000) //avançar o tempo em 3 segundos
+    cy.get('.success').should('not.be.visible')
+
   })
 
   it('marca todos os radio button', function () {
@@ -144,25 +148,25 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         expect($input[0].files[0].name).to.eq('example.json')
       })
   })
-  it('verifica que o link abre em outra aba', function(){
+  it('verifica que o link abre em outra aba', function () {
     cy.get('#privacy > a').should('have.attr', 'target', '_blank')
   })
 
-  it('abre o link que abriria em outra aba na mesma aba', function(){
+  it('abre o link que abriria em outra aba na mesma aba', function () {
     cy.get('#privacy > a')
-    .invoke('removeAttr', 'target')
-    .click()
+      .invoke('removeAttr', 'target')
+      .click()
     cy.contains('Talking About Testing').should('be.visible')
     cy.get('#white-background > p:nth-child(1)')
-    .should('have.text', 'Não salvamos dados submetidos no formulário da aplicação CAC TAT.')
+      .should('have.text', 'Não salvamos dados submetidos no formulário da aplicação CAC TAT.')
   })
 
-// Funçao que verifica se o link abre em uma nova aba
+  // Funçao que verifica se o link abre em uma nova aba
   function opensInNewTab(link) {
     return link.attr('target') === '_blank';
   }
-  
-  it('verifica que o link abre em outra aba', function() {
+
+  it('verifica que o link abre em outra aba', function () {
     cy.get('#privacy > a').then(($link) => {
       if (opensInNewTab($link)) {
         // Remove o atributo "target" do link
@@ -172,7 +176,87 @@ describe('Central de Atendimento ao Cliente TAT', function () {
       cy.wrap($link).click();
     });
   });
-  
-  
+
+
+  //executa o mesmo teste 5 vezes
+  Cypress._.times(5, () => {
+    it('testando a funcao cypress time', function () {
+      cy.get('#firstName').type('Fernando')
+      cy.get('#lastName').type('Silva')
+      cy.get('#email').type('fernando@gmail.com', { delay: 0 })
+      cy.get('select').select('cursos')
+      cy.get('[type="radio"]').check('elogio')
+      cy.get('#open-text-area').type('Teste')
+      cy.contains('button', 'Enviar').click()
+      cy.get('.success').should('be.visible')
+    })
+  })
+
+  it('testar o comando ctrl c ctrl v', function () {
+    const texto = Cypress._.repeat('teste teste ', 20)
+    cy.get('#firstName').type('Fernando')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('fernando@gmail.com', { delay: 0 })
+    cy.get('select').select('cursos')
+    cy.get('[type="radio"]').check('elogio')
+    cy.get('#open-text-area')
+      .invoke('val', texto)
+      .should('have.value', texto)
+    cy.contains('button', 'Enviar').click()
+    cy.get('.success').should('be.visible')
+  })
+
+  it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+    cy.get('.success')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Mensagem enviada com sucesso.')
+      .invoke('hide')
+      .should('not.be.visible')
+    cy.get('.error')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Valide os campos obrigatórios!')
+      .invoke('hide')
+      .should('not.be.visible')
+  })
+
+  it.only('faz aparecer o gatinho', function () {
+    cy.get('#cat')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+    cy.get('#subtitle')
+      .invoke('text', 'Eu alterei o sub título')
+  });
+
+})
+
+describe('Teste de API', function () {
+
+  it('teste de api metodo 1', () => {
+
+    cy.request({
+      method: 'GET',
+      url: 'https://cac-tat.s3.eu-central-1.amazonaws.com/index.html'
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.statusText).to.be.equal('OK');
+      expect(response.body).to.include('CAC TAT');
+    })
+  })
+
+  it('teste de api metodo 2', function () {
+    cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+      .should(function (response) {
+        // console.log(response) -> Verificar o log no inspecionar do browser
+        const { status, statusText, body } = response
+        expect(status).to.eq(200)
+        expect(statusText).to.eq('OK')
+        expect(body).to.include('CAC TAT')
+      })
+  })
 
 })
